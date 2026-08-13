0ebdc334be2222846168455aa832ac0fed56f08e
ricardoardon-cmd <ricardo.ardon@gmail.com>
2026-08-12 23:05:46 -0700

    Correct favoriteTeam value from 'nwe' to 'ne'

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -54,7 +54,7 @@ export const managers = [
       "bio": "Cottonwood",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2012, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nwe", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       //"mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Igoe", // Can be anything (usually your rival's name)
