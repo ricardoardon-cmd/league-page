3f9a5f20ccc09836bff7fbf12159827ca291498b
ricardoardon-cmd <ricardo.ardon@gmail.com>
2026-08-16 11:33:33 -0700

    Support hidden and external desktop nav links

--- src/lib/Nav/NavLarge.svelte
+++ src/lib/Nav/NavLarge.svelte
@@ -8,7 +8,6 @@
 	import { enableBlog, managers } from '$lib/utils/leagueInfo';
 
 	let active = $state(tabs.find(tab => tab.dest == page.url.pathname || (tab.nest && tab.children.find(subTab => subTab.dest == page.url.pathname))));
-
 	let display = $state(false);
 	let el = $state();
 	let width = $state();
@@ -19,12 +18,9 @@
 	$effect(() => {
 		top = el?.getBoundingClientRect() ? el?.getBoundingClientRect().top  : 0;
 		const bottom = el?.getBoundingClientRect() ? el?.getBoundingClientRect().bottom  : 0;
-
 		height = bottom - top + 1;
-
 		left = el?.getBoundingClientRect() ? el?.getBoundingClientRect().left  : 0;
 		const right = el?.getBoundingClientRect() ? el?.getBoundingClientRect().right  : 0;
-
 		width = right - left;
 	});
 
@@ -34,19 +30,21 @@
 		display = !display;
 	}
 
-	const subGoto = (dest) => {
-		open(false);
-		goto(dest);
+	const subGoto = (tab) => {
+		display = false;
+		if (tab.external) {
+			window.location.href = tab.dest;
+			return;
+		}
+		goto(tab.dest);
 	}
 
 	let tabChildren = $state([]);
-
 	for(const tab of tabs) {
-		if(tab.nest) {
-			tabChildren = tab.children;
-		}
+		if(tab.nest) tabChildren = tab.children;
 	}
 
+	const visibleChildren = () => tabChildren.filter(tab => !tab.hidden && (tab.label !== 'Managers' || managers.length));
 </script>
 
 <svelte:window bind:innerWidth={innerWidth} />
@@ -64,9 +62,7 @@
 		width: 22px;
 	}
 
-	.parent {
-		position: relative;
-	}
+	.parent { position: relative; }
 
 	.subMenu {
 		overflow-y: hidden;
@@ -83,74 +79,53 @@
 		top: 0;
 		left: 0;
 		width: 100%;
-		height: 100%;
 		height: 100vh;
 		z-index: 4;
 	}
 
-	:global(.mdc-deprecated-list) {
-		padding: 0;
-	}
-
-	:global(.subText) {
-		font-size: 0.8em;
-	}
-
-	:global(.dontDisplay) {
-		display: none;
-	}
+	:global(.mdc-deprecated-list) { padding: 0; }
+	:global(.subText) { font-size: 0.8em; }
+	:global(.dontDisplay) { display: none; }
 </style>
 
-<div tabindex="0" role="button" class="overlay" style="display: {display ? "block" : "none"};" onclick={() => open(true)}></div>
+<div tabindex="0" role="button" class="overlay" style="display: {display ? "block" : "none"};" onclick={() => display = false}></div>
 
 <div class="parent">
 	<TabBar class="navBar" {tabs} key={(tab) => tab.key} bind:active>
 		{#snippet tab(tab)}
-			{#if tab.nest}
-				<div bind:this={el}>
+			{#if !tab.hidden}
+				{#if tab.nest}
+					<div bind:this={el}>
+						<Tab {tab} minWidth onclick={() => open()}>
+							<Icon class="material-icons">{tab.icon}</Icon>
+							<Label>{tab.label}</Label>
+						</Tab>
+					</div>
+				{:else}
 					<Tab
+						class="{tab.label == 'Blog' && !enableBlog ? 'dontDisplay' : ''}"
 						{tab}
+						onTouchstart={() => {if(!tab.external) preloadData(tab.dest)}}
+						onMouseover={() => {if(!tab.external) preloadData(tab.dest)}}
+						href={tab.dest}
 						minWidth
-						onclick={() => open()}
 					>
 						<Icon class="material-icons">{tab.icon}</Icon>
 						<Label>{tab.label}</Label>
 					</Tab>
-				</div>
-			{:else}
-				<Tab
-					class="{tab.label == 'Blog' && !enableBlog ? 'dontDisplay' : ''}"
-					{tab}
-					onTouchstart={() => preloadData(tab.dest)}
-					onMouseover={() => preloadData(tab.dest)}
-					href={tab.dest}
-					minWidth
-				>
-					<Icon class="material-icons">{tab.icon}</Icon>
-					<Label>{tab.label}</Label>
-				</Tab>
+				{/if}
 			{/if}
 		{/snippet}
 	</TabBar>
-	<div class="subMenu" style="max-height: {display ? 49 * tabChildren.length - 1 - (managers.length ? 0 : 48) : 0}px; width: {width}px; top: {height}px; left: {left}px; box-shadow: 0 0 {display ? "3px" : "0"} 0 #00316b; border: {display ? "1px" : "0"} solid #00316b; border-top: none;">
+	<div class="subMenu" style="max-height: {display ? 49 * visibleChildren().length - 1 : 0}px; width: {width}px; top: {height}px; left: {left}px; box-shadow: 0 0 {display ? "3px" : "0"} 0 #00316b; border: {display ? "1px" : "0"} solid #00316b; border-top: none;">
 		<List>
-			{#each tabChildren as subTab, ix}
-				{#if subTab.label == 'Managers'}
-					<Item class="{managers.length ? '' : 'dontDisplay'}" onSMUIAction={() => subGoto(subTab.dest)} ontouchstart={() => preloadData(subTab.dest)} onmouseover={() => preloadData(subTab.dest)}>
-						<Graphic class="material-icons">{subTab.icon}</Graphic>
-						<Text class="subText">{subTab.label}</Text>
-					</Item>
-					{#if ix != tabChildren.length - 1}
-						<Separator />
-					{/if}
-				{:else}
-					<Item onSMUIAction={() => subGoto(subTab.dest)} ontouchstart={() => {if(subTab.label != 'Go to Sleeper') preloadData(subTab.dest)}} onmouseover={() => {if(subTab.label != 'Go to Sleeper') preloadData(subTab.dest)}}>
-						<Graphic class="material-icons">{subTab.icon}</Graphic>
-						<Text class="subText">{subTab.label}</Text>
-					</Item>
-					{#if ix != tabChildren.length - 1}
-						<Separator />
-					{/if}
+			{#each visibleChildren() as subTab, ix}
+				<Item onSMUIAction={() => subGoto(subTab)} ontouchstart={() => {if(!subTab.external) preloadData(subTab.dest)}} onmouseover={() => {if(!subTab.external) preloadData(subTab.dest)}}>
+					<Graphic class="material-icons">{subTab.icon}</Graphic>
+					<Text class="subText">{subTab.label}</Text>
+				</Item>
+				{#if ix != visibleChildren().length - 1}
+					<Separator />
 				{/if}
 			{/each}
 		</List>
