f276e704e50ca816d549af4cc7e28462ea0ae8f9
ricardoardon-cmd <ricardo.ardon@gmail.com>
2026-09-06 17:37:20 -0700

    Show latest completed draft first on Draft Center

--- src/routes/drafts/+page.js
+++ src/routes/drafts/+page.js
@@ -1,13 +1,11 @@
-import { getUpcomingDraft, getPreviousDrafts, getLeagueTeamManagers, loadPlayers } from '$lib/utils/helper';
+import { getPreviousDrafts, getLeagueTeamManagers, loadPlayers } from '$lib/utils/helper';
 
 export async function load({ fetch }) {
-    const upcomingDraftData = getUpcomingDraft();
     const previousDraftsData = getPreviousDrafts();
     const leagueTeamManagersData = getLeagueTeamManagers();
     const playersData = loadPlayers(fetch);
 
     return {
-        upcomingDraftData,
         previousDraftsData,
         leagueTeamManagersData,
         playersData,
